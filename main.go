ed4ed25c5d0e1dbf76cdda401223b459e68c9500
rohit katre <techqudra.rohit@gmail.com>
2025-11-12 11:18:26 +0530

    added timer for the quiz

--- main.go
+++ main.go
@@ -3,10 +3,14 @@ package main
 import (
 	"embed"
 	"encoding/csv"
+	"flag"
 	"fmt"
 	"io"
 	"log"
+	"math/rand"
+	"os"
 	"strings"
+	"time"
 )
 
 //go:embed data
@@ -47,6 +51,9 @@ func read_csv(file_path string) ([]Quiz, error) {
 }
 
 func main() {
+	quizTime := flag.Int("quiz-timer", 2, "duration of the quiz, default is 30s")
+	shuffel := flag.Bool("shuffel", false, "Shuffel the quiz question, defalult is false")
+
 	filepath := "data/problems.csv"
 	fileData, fileReadErr := read_csv(filepath)
 	if fileReadErr != nil {
@@ -58,6 +65,39 @@ func main() {
 
 	println("Please provide the answers to the corrusponding questions...\n")
 
+	quizTimer := time.NewTimer(time.Duration(*quizTime) * time.Second)
+
+	if *shuffel {
+		// Shuffle the slice
+		rand.Shuffle(len(fileData), func(i, j int) {
+			fileData[i], fileData[j] = fileData[j], fileData[i]
+		})
+	}
+
+	var done = make(chan bool)
+
+	fmt.Println("Starting a timer for the quiz")
+
+	go func() {
+
+		select {
+		case <-quizTimer.C:
+			fmt.Printf("Quiz time %ds is over, clossing the quiz\n", *quizTime)
+			fmt.Printf("Total Questions: %d\n", questionCount)
+			fmt.Printf("Correct Answer: %d\n", correctAnswerCount)
+			quizTimer.Stop()
+			os.Exit(0)
+		case <-done:
+			quizTimer.Stop() // stop the timer safely
+		}
+
+		// <-time.After(time.Duration(*quizTime) * time.Second)
+		fmt.Printf("Quiz time %ds is over, clossing the quiz\n", *quizTime)
+		fmt.Printf("Total Questions: %d\n", questionCount)
+		fmt.Printf("Correct Answer: %d\n", correctAnswerCount)
+		os.Exit(0)
+	}()
+
 	for _, quiz := range fileData {
 		var userAnswer string
 		fmt.Println(fmt.Sprintf("Question: %s", quiz.Question))
@@ -68,7 +108,9 @@ func main() {
 			correctAnswerCount = correctAnswerCount + 1
 		}
 	}
-	fmt.Printf("Total Questions: %d", questionCount)
-	fmt.Printf("Correct Answer: %d", correctAnswerCount)
+
+	done <- true // stopping the timer
+	fmt.Printf("Total Questions: %d\n", questionCount)
+	fmt.Printf("Correct Answers: %d\n", correctAnswerCount)
 
 }
